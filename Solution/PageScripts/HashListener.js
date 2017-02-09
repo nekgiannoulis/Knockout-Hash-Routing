4b64d8868786cbc5b77dae82825d62961a954282
nekgiannoulis <nekgiannoulis@gmail.com>
2017-02-09 16:10:55 +0200

    initial proof of concept solution added

---
+++ Solution/PageScripts/HashListener.js
@@ -0,0 +1,109 @@
+﻿
+
+var HashListener = {
+    HistoryName: "History",
+    Registrations: [],
+
+    Register: function (parameterName, koObservable, changedCallback) {
+        var params = HashListener.GetUrlVars();
+        var val = params[parameterName];
+        if (val) {
+            koObservable(val);
+        }
+
+        koObservable.HashRegistration = {
+            ParameterName: parameterName,
+            KoObservable: koObservable,
+            ChangedCallback: changedCallback,
+            InitialValue: koObservable()
+        };
+        HashListener.Registrations.push(koObservable.HashRegistration);
+
+        koObservable.subscribe(function (newValue) {
+            var reg = koObservable.HashRegistration;
+            var hashes = HashListener.GetUrlVars()[reg.ParameterName];
+            if (!hashes && newValue == reg.InitialValue) {
+                return;
+            }
+
+            if (hashes != newValue) {
+                var url = HashListener.ConstructString();
+                history.pushState(null, HistoryName, url);
+                if (reg.ChangedCallback) {
+                    if (!hashes) {
+                        reg.ChangedCallback(reg.InitialValue, newValue);
+                    } else {
+                        reg.ChangedCallback(hashes, newValue);
+                    }
+                }
+            }
+        });
+
+        window.onpopstate = function (e) {
+            var vars = HashListener.GetUrlVars();
+            $.each(HashListener.Registrations, function (index, reg) {
+                var paramValue = vars[reg.ParameterName];
+                if (!paramValue) {
+                    if (reg.InitialValue != reg.KoObservable()) {
+                        if (reg.ChangedCallback) {
+                            reg.ChangedCallback(reg.KoObservable(), reg.InitialValue);
+                        }
+                        reg.KoObservable(reg.InitialValue);
+                    }
+                } else if (paramValue != reg.KoObservable()) {
+                    if (reg.ChangedCallback) {
+                        reg.ChangedCallback(reg.KoObservable(), paramValue);
+                    }
+                    reg.KoObservable(paramValue);
+                }
+            });
+        }
+
+        if (koObservable()) {
+            if (koObservable.HashRegistration.ChangedCallback) {
+                koObservable.HashRegistration.ChangedCallback(null, koObservable());
+            }
+        }
+    },
+
+    ConstructString: function () {
+        var url = '?';
+        var found = [];
+        $.each(HashListener.Registrations, function (index, reg) {
+            if (reg.KoObservable()) {
+                if (index > 0)
+                    url += "&";
+                url += reg.ParameterName + "=" + reg.KoObservable();
+                found.push(reg.ParameterName);
+            }
+        });
+
+        var currentVars = HashListener.GetUrlVars();
+        $.each(currentVars, function (index, rec) {
+            if (found.indexOf(rec) < 0) {
+                url += "&" + rec + "=" + currentVars[rec];
+            }
+        });
+
+        if (url == '?')
+            return null;
+        return url;
+    },
+
+    //Read a page's GET URL variables and return them as an associative array.
+    GetUrlVars: function () {
+        console.log("Getting urls");
+        var vars = [], hash;
+        var qIndex = window.location.href.indexOf('?');
+        if (qIndex < 0)
+            return vars;
+
+        var hashes = window.location.href.slice(qIndex + 1).split('&');
+        for (var i = 0; i < hashes.length; i++) {
+            hash = hashes[i].split('=');
+            vars.push(hash[0]);
+            vars[hash[0]] = hash[1];
+        }
+        return vars;
+    }
+}
---
+++ Solution/PageScripts/TestBindings.js
@@ -0,0 +1,37 @@
+﻿
+$(document).ready(function () {
+
+    var vm = {
+        Test: ko.observable("test value"),
+        Test2: ko.observable("test value 2"),
+
+        UpdateTest: function () {
+
+            vm.Test("12334234sdf");
+        },
+
+        UpdateTest2: function () {
+            vm.Test2("NEKNEK");
+        },
+
+        GeneralTesting: function () {
+            history.pushState(null, "Project - ", "?generalTest=5");
+        }
+    };
+
+    HashListener.Register("test", vm.Test,
+    function (oldValue, newValue) {
+        var lala = newValue;
+    });
+
+
+    HashListener.Register("test22", vm.Test2,
+    function (oldValue, newValue) {
+        var lala = newValue;
+    });
+
+
+    ko.applyBindings(vm);
+});
+
+
